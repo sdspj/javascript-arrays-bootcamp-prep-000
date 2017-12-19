77862e196fa43e9943c5a2d82ddadd3b5a17be7a
Philly Kelly <p_a_k_@hotmail.com>
2017-12-19 01:48:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ chocolateBars = ["snicks", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
   [element, ...array]
 }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
