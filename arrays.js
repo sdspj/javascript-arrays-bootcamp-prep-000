2a3939f7654f7669fc21103dcd94a60899d4f6b0
Philly Kelly <p_a_k_@hotmail.com>
2017-12-19 01:57:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [element, ...array]
+  array = [element, ...array]
   return array
 }
